e8a27f301ed184ae8285ca5c72b9b8c42db92897
Peter Douglas <pdouglas811@gmail.com>
2023-02-27 11:22:29 +0000

    Update omnidir_test.py
    Updated the code to work with the latest version of opencv from 4.2 to 4.6

--- omnidir_test.py
+++ omnidir_test.py
@@ -1,48 +1,56 @@
-import numpy as np
 import cv2
+import numpy as np
 
-# Define the number of calibration images
-num_calibration_images = 53
+# Set up calibration parameters
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+objpoints = []  # 3D points in real world space
+imgpoints1 = []  # 2D points in image plane for camera 1
+imgpoints2 = []  # 2D points in image plane for camera 2
 
-# Define the number of corners in the calibration pattern
-pattern_size = (6, 8)
+# Define omnidirectional camera model
+K1 = np.array([[fx1, 0, cx1], [0, fy1, cy1], [0, 0, 1]])
+xi1 = np.array([xi1])
+D1 = np.array([k11, k12, k13, k14])
+camera_matrix1 = K1
+dist_coeffs1 = D1
 
-# Define the size of each square on the calibration pattern in meters
-square_size = 0.0808
+K2 = np.array([[fx2, 0, cx2], [0, fy2, cy2], [0, 0, 1]])
+xi2 = np.array([xi2])
+D2 = np.array([k21, k22, k23, k24])
+camera_matrix2 = K2
+dist_coeffs2 = D2
 
-# Define the omnidirectional camera model
-omnidir_model = cv2.omnidir.DUAL_EQUIRECTANGULAR
+# Load calibration images
+images1 = []
+images2 = []
+for i in range(1, num_images+1):
+    img1 = cv2.imread('camera1_%02d.png' % i)
+    img2 = cv2.imread('camera2_%02d.png' % i)
+    images1.append(img1)
+    images2.append(img2)
 
-# Define the calibration flags
-calib_flags = cv2.omnidir.CALIB_USE_GUESS + cv2.omnidir.CALIB_FIX_SKEW
-
-# Create arrays to store the object points and image points for each calibration image
-object_points = []
-image_points_left = []
-image_points_right = []
-
-# Generate the object points for the calibration pattern
-objp = np.zeros((pattern_size[0] * pattern_size[1], 3), np.float32)
+# Find chessboard corners in calibration images
+pattern_size = (chessboard_cols, chessboard_rows)
+objp = np.zeros((pattern_size[0]*pattern_size[1], 3), np.float32)
 objp[:, :2] = np.mgrid[0:pattern_size[0], 0:pattern_size[1]].T.reshape(-1, 2)
-objp *= square_size
+for i in range(num_images):
+    img1 = images1[i]
+    img2 = images2[i]
+    gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    found1, corners1 = cv2.findChessboardCorners(gray1, pattern_size, None)
+    found2, corners2 = cv2.findChessboardCorners(gray2, pattern_size, None)
 
-# Load the calibration images from both cameras
-for i in range(num_calibration_images):
-    img_left = cv2.imread(f"/media/AC10-0657/Images/Set_1/Left/{i}")
-    img_right = cv2.imread(f"/media/AC10-0657/Images/Set_1/Right{i}")
+    # Add object points and image points to lists
+    if found1 and found2:
+        objpoints.append(objp)
+        corners1 = cv2.cornerSubPix(gray1, corners1, (11, 11), (-1, -1), criteria)
+        imgpoints1.append(corners1)
+        corners2 = cv2.cornerSubPix(gray2, corners2, (11, 11), (-1, -1), criteria)
+        imgpoints2.append(corners2)
 
-    # Find the corners of the calibration pattern in both images
-    ret_left, corners_left = cv2.findChessboardCorners(img_left, pattern_size)
-    ret_right, corners_right = cv2.findChessboardCorners(img_right, pattern_size)
-
-    # If the corners are found in both images, add the object and image points to their respective arrays
-    if ret_left and ret_right:
-        object_points.append(objp)
-        image_points_left.append(corners_left)
-        image_points_right.append(corners_right)
-
-# Perform stereo calibration to obtain the intrinsic and extrinsic parameters of the cameras
-ret, K_left, D_left, K_right, D_right, R, T = cv2.omnidir.stereoCalibrate(object_points, image_points_left, image_points_right, None, None, None, None, img_left.shape[::-1], omnidir_model, calib_flags)
-
-# Save the calibration parameters to a file
-np.savez('calibration_params.npz', K_left=K_left, D_left=D_left, K_right=K_right, D_right=D_right, R=R, T=T)
+# Perform stereo calibration
+criteria_calib = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 1e-5)
+flags = cv2.omnidir.CALIB_FIX_SKEW + cv2.omnidir.CALIB_USE_GUESS + cv2.omnidir.CALIB_FIX_PRINCIPAL_POINT
+(rms, K, xi, D, rvecs, tvecs) = cv2.omnidir.stereoCalibrate(
+    objpoints, imgpoints1, imgpoints2, camera_matrix
